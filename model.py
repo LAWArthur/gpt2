a006b67dc1ba42df469a3666eae72e1ab930f691
LAWArthur <jincai3335_micro@163.com>
2024-07-12 15:30:01 +0800

    wowowo

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 @dataclass
 class GPTConfig:
     block_size: int = 1024 # tmax
-    vocab_size: int = 50257
+    vocab_size: int = 50304
     n_layer: int = 12
     n_head: int = 12
     n_embd:int = 768
