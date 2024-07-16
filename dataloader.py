48419768c1bb2a2c431eeda722098043991920f2
LAWArthur <jincai3335_micro@163.com>
2024-07-16 19:41:07 +0800

    added dataset

--- dataloader.py
+++ dataloader.py
@@ -1,15 +1,19 @@
 import tiktoken
 import torch
+import numpy as np
 class DataLoader:
     def __init__(self, B, T):
         self.B = B
         self.T = T
+        # with open('tinyshakespeare/input.txt', 'r') as f:
+        #     text = f.read()
+        # enc = tiktoken.get_encoding('gpt2')
+        # tokens = enc.encode(text)
+        # self.tokens = torch.tensor(tokens)
+        # print(f'loaded {len(self.tokens)} tokens.')
+        # print(f'1 epoch = {len(self.tokens) //  (B*T) } batches.')
 
-        with open('tinyshakespeare/input.txt', 'r') as f:
-            text = f.read()
-        enc = tiktoken.get_encoding('gpt2')
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
+        self.tokens = np.memmap('train.bin', dtype=np.uint16, mode='r')
         print(f'loaded {len(self.tokens)} tokens.')
         print(f'1 epoch = {len(self.tokens) //  (B*T) } batches.')
 
@@ -18,6 +22,7 @@ class DataLoader:
     def next_batch(self):
         B,T = self.B, self.T
         buf = self.tokens[self.current_position : self.current_position + B*T + 1]
+        buf = torch.tensor(buf)
         x = (buf[:-1]).view(B,T)
         y = (buf[1:]).view(B,T)
 
