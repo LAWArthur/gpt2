636d1d57290c0a45416b484d61f6988c0c2d017c
LAWArthur <jincai3335_micro@163.com>
2024-07-20 22:47:20 +0800

    chinese dataset

--- fineweb.py
+++ fineweb.py
@@ -16,11 +16,14 @@ num_proc = 8
 # it is better than 1 usually though
 num_proc_load_dataset = num_proc
 
+dataset_path = 'b3x0m/Chinese-H-Novels'
+dataset_dir = 'data/chinese_h_novels_tokens'
+
 enc = tiktoken.get_encoding("gpt2")
 
 if __name__ == '__main__':
     # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
-    dataset = load_dataset("HuggingFaceFW/fineweb-edu", name='sample-10BT', num_proc=num_proc_load_dataset)
+    dataset = load_dataset(dataset_path, num_proc=num_proc_load_dataset)
 
     # owt by default only contains the 'train' split, so create a test split
     split_dataset = dataset['train'].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
@@ -57,7 +60,7 @@ if __name__ == '__main__':
     # concatenate all the ids in each dataset into one large file we can use for training
     for split, dset in tokenized.items():
         arr_len = np.sum(dset['len'], dtype=np.uint64)
-        filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
+        filename = os.path.join(dataset_path, f'{split}.bin')
         dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
         arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
         total_batches = 1024
